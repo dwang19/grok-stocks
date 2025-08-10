d0df86d8c7e2b243ea00bee039bd968749b406ad
dwang19 <lijen_dwang@hotmail.com>
2025-08-09 22:08:51 -0700

    Added __init__.py for the widgets directory

--- view/macro_dashboard.py
+++ view/macro_dashboard.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 def show_macro_dashboard():
     st.title("Macro Dashboard")
-    st.write("This is the Macro Dashboard page. Widgets coming soon!")
+    st.write("Overview of market environment for position trades.")
+    # Widgets will be added here, e.g.:
+    # from .widgets.treasury_yield_widget import treasury_yield_widget
+    # treasury_yield_widget('your_api_key')
